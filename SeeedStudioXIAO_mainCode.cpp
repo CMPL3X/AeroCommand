2790b7e95abe77b55f469e61e88de98cba0c2694
Gustavs <87529263+CMPL3X@users.noreply.github.com>
2024-08-23 20:24:24 +0300

    Create SeeedStudioXIAO_mainCode.cpp

---
+++ SeeedStudioXIAO_mainCode.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <PocketSphinx.h>
+
+const int rxPin = D7;
+const int txPin = D6;
+
+SoftwareSerial mySerial(rxPin, txPin);
+
+PocketSphinx sphinx;
+
+const char* keywords[] = {"stop!", "drone up", "drone down", "drone left", "drone right", "drone rotate left", "drone rotate right", "face control on", "face control off", "takeoff", "land", "come home"};
+
+void setup() {
+  mySerial.begin(9600);
+  sphinx.init();
+  sphinx.start();
+}
+
+void loop() {
+  String command = sphinx.recognize();
+  if (command != "") {
+    for (int i = 0; i < sizeof(keywords) / sizeof(keywords[0]); i++) {
+      if (command.equalsIgnoreCase(keywords[i])) {
+        mySerial.println(command);
+        break;
+      }
+    }
+  }
+}
